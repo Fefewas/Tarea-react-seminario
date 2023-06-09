377b56ba1377e1e29ac57606d794d04d9d85f12d
Jarid Osorio <jariddavid78@gmail.com>
2023-06-08 20:27:55 -0500

    Arreglo Galeria

--- src/components/galleryExpandedView.jsx
+++ src/components/galleryExpandedView.jsx
@@ -1,37 +1,20 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import { useState } from 'react';
+import React from 'react';
 
 import '../stylesheets/galleryExpandedView.css';
 
 const GalleryExpandedViwe = ({ images }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const openImage = (image) => {
-    setSelectedImage(image);
-  };
-
-  const closeImage = () => {
-    setSelectedImage(null);
-  };
 
   return (
     <div>
       <div className="gallery">
-  {images.map((image, index) => (
+        {images.map((image, index) => (
     <div key={index} className="image-item">
-      <img src={image} alt={`Image ${index}`} onClick={() => openImage(image)} />
+      <img src={image} alt={`Image ${index}`} />
     </div>
   ))}
 </div>
 
-      {selectedImage && (
-        <div className="overlay">
-          <div className="modal">
-            <img src={selectedImage} alt="Selected Image" />
-            <button onClick={closeImage}>Cerrar</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
